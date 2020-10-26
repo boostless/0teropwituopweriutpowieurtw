1a18e7a9049b2daa0619657f7acb3dd43ed3ede5
boostless <46201063+boostless@users.noreply.github.com>
2020-10-26 20:11:14 +0200

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args)=>{
   let ipEmbed = new Discord.RichEmbed()
   .setTitle("**Serverio IP**")
-  .addField("``185.150.25.92:30292``", "SuncityRP")
+  .addField("``89.40.2.59:30121``", "SuncityRP")
   .setColor("#f54242");
   message.channel.send(ipEmbed);
 }
